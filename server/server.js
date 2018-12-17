9278a3863ef970d97bfbfc3e4fa22f27f3e0a148
Deividas Urbonavicius <u.deividas@hotmail.com>
2018-12-17 12:06:45 +0200

    add todo update PATCH

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const _ = require('lodash');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
 
@@ -71,6 +72,30 @@ app.delete('/todos/:id', (req, res) => {
             }
             console.log('deleting todo item');
             res.status(200).send({todo})
+        })
+        .catch(e => res.status(400).send())
+});
+
+app.patch('/todos/:id', (req, res) => {
+    const id = req.params.id;
+    const body = _.pick(req.body, ['text', 'completed']);
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send()
+    }
+
+    if (_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, {$set:  body}, {new: true})
+        .then(todo => {
+            if (!todo) return res.status(404).send();
+
+            res.send({todo});
         })
         .catch(e => res.status(400).send())
 })
